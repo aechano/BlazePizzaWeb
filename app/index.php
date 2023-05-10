7486c299e68d56f649bc7d43ab9ac53258412a50
Renze Meinard Mortiga <98935669+renzewithane@users.noreply.github.com>
2023-05-10 23:10:48 +0800

    Added Latest Reviews on Homepage

--- app/index.php
+++ app/index.php
@@ -77,6 +77,114 @@
                         <img src="../assets/promos/Instagram_Ad.png" alt="Instagram Ad" class="ad-image" />
                     </a>
                 </section>
+
+                <section>
+                    <br><br>
+                    <div id="reviews-box">
+                        <h3>Customer Reviews</h3>
+                        <h5>See the latest reviews of Blaze Pizza Red Deer:</h5>
+                        <div class="reviews">
+                            <div class="review">
+                            <div class="review-header">
+                                <h4>John Doe</h4>
+                                <div class="rating">
+                                <span class="star">&#9733;</span>
+                                <span class="star">&#9733;</span>
+                                <span class="star">&#9733;</span>
+                                <span class="star">&#9733;</span>
+                                <span class="star">&#9733;</span>
+                                </div>
+                            </div>
+                            <p>"Blaze Pizza is amazing! I love the selection of toppings and the crust is perfect."</p>
+                            </div>
+                            
+                            <div class="review">
+                            <div class="review-header">
+                                <h4>Jane Smith</h4>
+                                <div class="rating">
+                                <span class="star">&#9733;</span>
+                                <span class="star">&#9733;</span>
+                                <span class="star">&#9733;</span>
+                                <span class="star">&#9733;</span>
+                                <span class="star empty">&#9733;</span>
+                                </div>
+                            </div>
+                            <p>"Great pizza, but the service was a bit slow. Would come back for the food though."</p>
+                            </div>
+                            
+                            <div class="review">
+                            <div class="review-header">
+                                <h4>Bob Johnson</h4>
+                                <div class="rating">
+                                <span class="star">&#9733;</span>
+                                <span class="star">&#9733;</span>
+                                <span class="star">&#9733;</span>
+                                <span class="star empty">&#9733;</span>
+                                <span class="star empty">&#9733;</span>
+                                </div>
+                            </div>
+                            <p>"Decent pizza, but nothing special. Could use more seasoning in the sauce."</p>
+                            </div>
+
+                            <div class="review">
+                            <div class="review-header">
+                                <h4>Mary Smith</h4>
+                                <div class="rating">
+                                <span class="star">&#9733;</span>
+                                <span class="star">&#9733;</span>
+                                <span class="star">&#9733;</span>
+                                <span class="star">&#9733;</span>
+                                <span class="star empty">&#9733;</span>
+                                </div>
+                            </div>
+                            <p>"Blaze Pizza is great! The pizzas are delicious and the staff is always friendly. I deducted one star because the restaurant can get a bit crowded at peak times."</p>
+                            </div>
+
+                            <div class="review">
+                            <div class="review-header">
+                                <h4>Alex Rodriguez</h4>
+                                <div class="rating">
+                                <span class="star">&#9733;</span>
+                                <span class="star">&#9733;</span>
+                                <span class="star">&#9733;</span>
+                                <span class="star">&#9733;</span>
+                                <span class="star">&#9733;</span>
+                                </div>
+                            </div>
+                            <p>"I'm a big fan of Blaze Pizza. The crust is crispy and the toppings are fresh and flavorful. The staff is always attentive and the service is quick. Highly recommend!"</p>
+                            </div>
+
+                            <div class="review">
+                            <div class="review-header">
+                                <h4>Emily Johnson</h4>
+                                <div class="rating">
+                                <span class="star">&#9733;</span>
+                                <span class="star">&#9733;</span>
+                                <span class="star">&#9733;</span>
+                                <span class="star empty">&#9733;</span>
+                                <span class="star empty">&#9733;</span>
+                                </div>
+                            </div>
+                            <p>"I had high hopes for Blaze Pizza, but was a bit disappointed. The pizza was decent, but nothing special. The restaurant was also quite loud and crowded. Not sure if I would return."</p>
+                            </div>
+
+                            <div class="review">
+                            <div class="review-header">
+                                <h4>Tom Lee</h4>
+                                <div class="rating">
+                                <span class="star">&#9733;</span>
+                                <span class="star">&#9733;</span>
+                                <span class="star">&#9733;</span>
+                                <span class="star">&#9733;</span>
+                                <span class="star">&#9733;</span>
+                                </div>
+                            </div>
+                            <p>"Blaze Pizza is one of my favorite pizza places. I love the option to create my own pizza and choose from a variety of toppings. The staff is always friendly and the service is fast. Highly recommend!"</p>
+                            </div>
+                        </div>
+                        </div>
+
+                </section>
             </div>
             
             <!-- products ends here -->
