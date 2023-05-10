66938f0d5853c261f3911df31eca57e3d8f39be2
Renze Meinard Mortiga <98935669+renzewithane@users.noreply.github.com>
2023-05-10 23:46:39 +0800

    fixed account settings

--- app/account_settings.php
+++ app/account_settings.php
@@ -21,29 +21,37 @@
             <div class="account-info">
                 <div class="indiv-info">
                     <p class="acc-info-title">First Name:</p>
-                    <p class="acc-info-content">Angelo</p>
+                    <textarea type="text" class="acc-info-content" readonly>Angelo</textarea>
                 </div>
                 <div class="indiv-info">
                     <p class="acc-info-title">Last Name:</p>
-                    <p class="acc-info-content">Echano</p>
+                    <textarea type="text" class="acc-info-content" readonly>Echano</textarea>
                 </div>
                 <div class="indiv-info">
                     <p class="acc-info-title">Email Address:</p>
-                    <p class="acc-info-content">angelo.echano@example.ca</p>
+                    <textarea type="text" class="acc-info-content" readonly>angelo.echano@example.ca</textarea>
+
                 </div>
                 <div class="indiv-info">
                     <p class="acc-info-title">Phone Number:</p>
-                    <p class="acc-info-content">(403) 555-1234)</p>
+                    <textarea type="text" class="acc-info-content" readonly>(403) 555-1234</textarea>
                 </div>
                 <div class="indiv-info">
                     <p class="acc-info-title">Address:</p>
-                    <p class="acc-info-content">123 Main Street, Red Deer, AB T4N 2R2, Canada</p>
+                    <textarea type="text" class="acc-info-content2" readonly>123 Main Street, Red Deer, AB T4N 2R2, Canada</textarea>
                 </div>
                 <div class="indiv-info">
+                    <p class="acc-info-title">E-Wallet:</p>
+                    <textarea type="text" class="acc-info-content" readonly>$999</textarea>
                 </div>
+                <!-- To add, just copy the code below
+                <div class="indiv-info">
+                    <p class="acc-info-title">Title goes here</p>
+                    <textarea type="text" class="acc-info-content" readonly>Content goes here</textarea>
+                </div>
+                -->
             </div>
         </div>
-        <?php include 'footer.php';?>
         <script type="text/javascript" src="../js/script.js"></script>
     </body>
 </html>
